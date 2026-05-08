6628e02d0f93c342efe4866be4b1526ccdf1197e
khushiipatel027-droid <khushiipatel027@gmail.com>
2026-05-08 15:47:34 +0530

    fix vercel app

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def resume():
         'resume.pdf',
         as_attachment=True
     )
+application = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
